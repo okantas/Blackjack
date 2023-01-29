160e00cbcc50e20cda4ffcf01da66444ade8b4fb
okantas <okantas954@gmail.com>
2023-01-29 14:47:13 +0300

    I developed it

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-let firstCard=8
+let firstCard=11 
 let secondCard=10
 let sum=firstCard+secondCard
 let message=" "
@@ -8,18 +8,22 @@ let hasBlackjack=false
 let isAlive=true
 
 let messageEl= document.getElementById("message_el")
+let sumEl=document.getElementById("sum_el")
+let cardsEl=document.getElementsById("cards_el")
 
 
-function startgame(){
+function startGame(){
+    cardsEl.textContent="Cards: "+ firstCard+" - "+ secondCard
+    sumEl.textContent= "SUM: "+sum
     if(sum<=20){
-        message="Do you want to draw a new card? 🙂"
+        message="Do you want to draw a new card?"
     }
     else if(sum===21){
-        message="Wohoo! You've got Blackjack! 🥳"
+        message="Wohoo! You've got Blackjack!"
         hasBlackjack=true
     }
     else{
-        message="You're out of the game! 😭"
+        message="You're out of the game!"
         isAlive=false
     }
     messageEl.textContent= message
